e7beca3ace956f3b8893d85660f81866fce06d0e
ke-pan <ke_pan@yeah.net>
2016-06-21 23:05:17 +0800

    feat: event index day and project

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -3,8 +3,26 @@ class EventDecorator < Draper::Decorator
 
   def generated_at
     helpers.content_tag :span, class: 'time' do
-      object.created_at.strftime("%H:%M")
+      object.created_at.strftime('%H:%M')
     end
   end
 
+  def daystamps
+    if object.created_at.midnight == today
+      '今天'
+    elsif object.created_at.midnight == yesterday
+      '昨天'
+    else
+      object.created_at.strftime('%-m/%-d')
+    end
+  end
+
+  private
+  def today
+    @today = Time.zone.today
+  end
+
+  def yesterday
+    @yesterday = Time.zone.yesterday
+  end
 end
